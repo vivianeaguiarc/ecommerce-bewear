b6b338ff673f3a39f1aec0e0d5d96c11865133da
Viviane Aguiar <vivianeaguiarc@outlook.com>
2025-09-21 09:09:57 -0300

    feat(category): implementar componente CategorySelector
    - Criado componente CategorySelector para exibir categorias
    - Renderização em grid 2 colunas com botões estilizados
    - Cada categoria exibida como botão arredondado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 
+import CategorySelector from "@/components/common/category-selector";
 import Header from "@/components/common/header";
 import ProductList from "@/components/common/product-list";
 import { db } from "@/db";
@@ -10,7 +11,7 @@ const Home = async () => {
       variants: true,
     },
   });
-  console.log(products);
+  const categories = await db.query.categoryTable.findMany({})
   return (
     <>
       <Header />
@@ -26,6 +27,9 @@ const Home = async () => {
           />
         </div>
         <ProductList products={products} title="Mais vendidos" />
+        <div className="px-5">
+          <CategorySelector categories={categories} />
+        </div>
         <div className="px-5">
           <Image
             src="/banner-1.png"
